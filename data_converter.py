84c7e5773e15798569b3814c8aa02d80dd1407e5
Cali0707 <65566908+Cali0707@users.noreply.github.com>
2020-10-25 17:37:16 -0400

    Cleaned up script

--- data_converter.py
+++ data_converter.py
@@ -3,13 +3,13 @@ import math
 
 
 def convert_degrees_to_radians(file_to_load, file_to_write, x_err, y_err):
-    deg = np.loadtxt(file_to_load, usecols=0, skiprows=1, unpack=True)
+    deg = np.loadtxt(file_to_load, usecols=(0, 1), unpack=True)
     f = open(file_to_write, 'a')
     f.write('x y dx dy')
     for i in range(len(deg[0])):
         x = deg[0][i]
         y = math.radians(deg[1][i])
-        s = str(x) + " " + str(y) + " " + str(x_err) + " " + str(y_err)
+        s = str(x) + " " + str(y) + " " + str(x_err) + " " + str(y_err) + "\n"
         f.write(s)
 
     f.close()
@@ -18,8 +18,8 @@ def convert_degrees_to_radians(file_to_load, file_to_write, x_err, y_err):
 if __name__ == '__main__':
     file_to_load = 'lab3_measurements.txt'
     file_to_write = 'lab3_in_radians.txt'
-    x_err = 0.1
-    y_err = 0.1
+    x_err = 0.0167
+    y_err = 0.00873
     convert_degrees_to_radians(file_to_load, file_to_write, x_err, y_err)
 
 
